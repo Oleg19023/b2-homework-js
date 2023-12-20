4aca2b49a7706349f163bf1f427f760923f3931b
Oleg19023 <96693781+Oleg19023@users.noreply.github.com>
2023-12-20 22:51:00 +0200

    commit-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,64 +9,54 @@ function numberToWords() {
 
     const number = parseInt(inputNumber);
 
-    if (isNaN(number) || number < 1 || number > 999) {
-        outputWords.textContent = "Введите корректное число от 1 до 999";
+    if (isNaN(number) || number < 1 || number > 999999999999) {
+        outputWords.textContent = "Введите корректное число от 1 до 999 999 999 999";
         return;
     }
 
-    if (number === 0) {
-        outputWords.textContent = "ноль гривен";
-        return;
-    }
-
-    const units = ["", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"];
+    const units = ["", "одна", "две", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"];
     const teens = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"];
     const tens = ["", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"];
     const hundreds = ["", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"];
+    const thousands = ["", "тысяча", "миллион", "миллиард"];
 
-    function convertUnits(num) {
-        return units[num];
+    function convert(num, idx) {
+        if (!num) return '';
+        let str = '';
+        if (num > 99) str += hundreds[Math.floor(num / 100)] + ' ';
+        if (num % 100 > 9 && num % 100 < 20) return str + teens[num % 10] + ' ' + thousands[idx] + ' ';
+        if (num % 100 > 9) str += tens[Math.floor((num % 100) / 10)] + ' ';
+        if (idx && num % 10 > 1 && num % 10 < 5) str += units[num % 10] + 'и' + ' ' + thousands[idx] + ' ';
+        else str += units[num % 10] + ' ' + thousands[idx] + ' ';
+        return str;
     }
 
-    function convertTeens(num) {
-        return teens[num - 10];
-    }
-
-    function convertTens(num) {
-        if (num >= 10 && num < 20) {
-            return convertTeens(num);
+    function getCurrencyDeclension(num) {
+        const lastDigit = num % 10;
+        const lastTwoDigits = num % 100;
+        if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+            return "гривен";
+        } else if (lastDigit === 1) {
+            return "гривна";
+        } else if (lastDigit >= 2 && lastDigit <= 4) {
+            return "гривны";
+        } else {
+            return "гривен";
         }
-        return tens[Math.floor(num / 10)];
     }
 
-    function convertHundreds(num) {
-        return hundreds[Math.floor(num / 100)];
+    let words = '';
+    let arr = [];
+    let tempNumber = number;
+    while (tempNumber > 0) {
+        arr.push(tempNumber % 1000);
+        tempNumber = Math.floor(tempNumber / 1000);
+    }
+    for (let i = arr.length - 1; i >= 0; i--) {
+        words += convert(arr[i], i) + ' ';
     }
 
-    function convertRest(num) {
-        if (num === 0) return "";
-        if (num < 10) return convertUnits(num);
-        if (num < 20) return convertTeens(num);
-        const restTens = convertTens(num);
-        const restUnits = convertUnits(num % 10);
-        return restTens + (restUnits ? ` ${restUnits}` : "");
-    }
-
-    let words = "";
-    const numArray = number.toString().split("").map(Number);
-
-    if (numArray.length === 1) {
-        words = convertUnits(numArray[0]);
-    } else if (numArray.length === 2) {
-        words = convertRest(number);
-    } else if (numArray.length === 3) {
-        const restNumber = Number(numArray.slice(1).join(""));
-        words = `${convertHundreds(numArray[0])} ${restNumber < 20 ? convertRest(restNumber) : convertRest(Number(numArray.slice(1).join("")))}`
-    } else {
-        words = "Превышен максимальный предел (999)";
-    }
-
-    outputWords.textContent = words + " гривен";
+    outputWords.textContent = words.trim() + " " + getCurrencyDeclension(number);
 }
 
 document.getElementById("inputNumber").addEventListener("input", numberToWords);
